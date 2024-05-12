f2c8c639838b78ffbcc5b96f2d8c4a089d2b95a2
kucingcoder <137396727+kucingcoder@users.noreply.github.com>
2024-05-12 20:54:03 +0700

    Membuat routing masuk dan keluar

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,14 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->group('', ['filter' => 'umum'], function ($routes) {
+    $routes->get('/', 'Home::Index');
+
+    $routes->post('/auth', 'Auth::Masuk');
+});
+
+$routes->group('', ['filter' => 'sesi'], function ($routes) {
+    $routes->get('/auth', 'Auth::Keluar');
+
+    $routes->get('/dashboard', 'Dashboard::Index');
+});
