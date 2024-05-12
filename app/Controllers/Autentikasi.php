9c43e71777f5c85b9ecfe9ad80d4b039a95da105
kucingcoder <137396727+kucingcoder@users.noreply.github.com>
2024-05-12 20:44:10 +0700

    membuat controller Autentikasi

---
+++ app/Controllers/Autentikasi.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use App\Models\ModelGuru;
+use CodeIgniter\HTTP\Request;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class Autentikasi extends BaseController
+{
+    public function Masuk(Request $request)
+    {
+        $Session = session();
+
+        $Username = $this->request->getVar("username");
+        $Password = md5($this->request->getVar("password"));
+
+        $ModelGuru = new ModelGuru;
+        $Guru = $ModelGuru->where(["username" => $Username, "password" => $Password])->first();
+
+        if (!$Guru) {
+            $Session->setFlashdata("msg", "Username atau Password salah");
+            return redirect()->to("/");
+        }
+
+        $Data = [
+            "nip"     => $Guru["nip"],
+            "inisial" => $Guru["inisial"],
+            "nama"    => $Guru["nama"],
+            "jenis"   => $Guru["jenis"],
+            "sesi"    => true
+        ];
+
+        $Session->set($Data);
+
+        return redirect()->to("/dashboard");
+    }
+
+    public function Keluar()
+    {
+        $Session = session();
+        $Session->destroy();
+        return redirect()->to("/");
+    }
+}
