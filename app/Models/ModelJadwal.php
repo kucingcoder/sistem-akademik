15774bdc5d9c5d35d8be857063b60f007ccba1d0
kucingcoder <137396727+kucingcoder@users.noreply.github.com>
2024-05-13 19:53:13 +0700

    Ubah cara pencarian data pelaksanaan KBM

--- app/Models/ModelJadwal.php
+++ app/Models/ModelJadwal.php
@@ -28,33 +28,13 @@ class ModelJadwal extends Model
         return $query->getResult();
     }
 
-    function PelaksanaanKBM($mapel, $kelas, $nip, $mulai)
+    function PelaksanaanKBM($id_ampu)
     {
-        $this->select("jadwal.mulai, jadwal.selesai as sampai, ampu_mapel.jumlah_jam");
+        $this->select("mata_pelajaran.nama_mapel AS nama_mapel, kelas.nama_kelas, ampu_mapel.jumlah_jam AS jumlah_jam");
         $this->join("ampu_mapel", "jadwal.ampu_mapel_id_ampu = ampu_mapel.id_ampu");
-        $this->where("jadwal.ampu_mapel_id_ampu", function ($subQuery) use ($mapel, $kelas, $nip) {
-            $subQuery->select("id_ampu")
-                ->from("ampu_mapel")
-                ->where("kelas_id_kelas", function ($subSubQuery) use ($kelas) {
-                    $subSubQuery->select("id_kelas")
-                        ->from("kelas")
-                        ->where("nama_kelas", $kelas);
-                })
-                ->where("semester_id_semester", function ($subSubQuery) {
-                    $subSubQuery->select("CASE WHEN MONTH(CURRENT_DATE) <= 6 THEN 1 ELSE 1 END");
-                })
-                ->where("mata_pelajaran_id_mapel", function ($subSubQuery) use ($mapel) {
-                    $subSubQuery->select("id_mapel")
-                        ->from("mata_pelajaran")
-                        ->where("nama_mapel", $mapel);
-                })
-                ->where("guru_inisial", function ($subSubQuery) use ($nip) {
-                    $subSubQuery->select("inisial")
-                        ->from("guru")
-                        ->where("nip", $nip);
-                });
-        });
-        $this->where("jadwal.mulai", "TIME_FORMAT('$mulai', '%H:%i:%s')", false);
+        $this->join("mata_pelajaran", "ampu_mapel.mata_pelajaran_id_mapel = mata_pelajaran.id_mapel");
+        $this->join("kelas", "ampu_mapel.kelas_id_kelas = kelas.id_kelas");
+        $this->where("ampu_mapel.id_ampu", $id_ampu);
 
         return $this->first();
     }
